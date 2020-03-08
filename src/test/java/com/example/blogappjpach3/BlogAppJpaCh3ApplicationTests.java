6a0e94da02d40555ba487bc1ac00e376363f96c3
Krzysztof Urban <k.urban@g.pl>
2020-03-08 10:10:59 +0100

    Initial commit

---
+++ src/test/java/com/example/blogappjpach3/BlogAppJpaCh3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blogappjpach3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogAppJpaCh3ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
